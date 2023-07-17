14c69830f815a19577f4a7537ae2d1cd6994e45b
Maximiliano Principe <mlapeducacionit@gmail.com>
2023-07-17 19:40:23 -0300

    Actualizo el vite.config.js para solucionar problemas del build

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
+import { resolve } from 'node:path'
+
+// console.log(resolve('views')) /* quiero saber, cual es la ruta completa (absoluta) donde view */
+
 export default {
   server: {
     port: "2222",
@@ -5,4 +9,16 @@ export default {
   css: {
     devSourcemap: true,
   },
+  build: {
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        alta: resolve('views/alta.html'),
+        carrito: resolve('views/carrito.html'),
+        contacto: resolve('views/contacto.html'),
+        nosotros: resolve('views/nosotros.html'),
+        inicio: resolve('index.html')
+      }
+    }
+  }
 };
